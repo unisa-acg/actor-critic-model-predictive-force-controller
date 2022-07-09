0388a9b6f4b371d7579710334cad94744f3e321d
Alessandro Pozzi <alessandro11.pozzi@mail.polimi.it>
2022-07-09 12:09:33 +0000

    [ADD] Added robosuite path setting

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
-export MJ_PANDA_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=$MJ_PANDA_PATH:$PYTHONPATH
-
-export MUJOCO_VALIDATION_PATH="$( cd "$( dirname "mujoco_validation" )" && pwd )"
+export MUJOCO_VALIDATION_PATH="$( cd "$( dirname "mujoco_validation" )" && pwd)"
 export PYTHONPATH=$MUJOCO_VALIDATION_PATH:$PYTHONPATH
 
-echo -e "Setting MJ_PANDA_PATH=$MJ_PANDA_PATH\n"
+export MJ_PANDA_PATH="$( cd "$( dirname "mujoco_panda_master/mujoco_panda" )" && pwd)"
+export PYTHONPATH=$MJ_PANDA_PATH:$PYTHONPATH
+
+export ROBOSUITE_PATH="$( cd "$( dirname "robosuite_master/robosuite" )" && pwd)"
+export PYTHONPATH=$ROBOSUITE_PATH:$PYTHONPATH
+
+echo -e "Setting MUJOCO_VALIDATION_PATH=$MUJOCO_VALIDATION_PATH\n"
+echo -e "Setting MUJOCO_PANDA_PATH=$MJ_PANDA_PATH\n"
+echo -e "Setting ROBOSUITE_PATH=$ROBOSUITE_PATH\n"
+echo -e "All set!\n"
